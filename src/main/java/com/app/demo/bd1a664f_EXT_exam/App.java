75f19e7d9e4d3bdf7479886d61e2e2b74c5eb027
Nanditha <nandithay2005@gmail.com>
2025-01-18 11:57:16 +0530

    commit

---
+++ src/main/java/com/app/demo/bd1a664f_EXT_exam/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.bd1a664f_EXT_exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "22bd1a664f_External_exam 18/1/2025" );
+    }
+}
